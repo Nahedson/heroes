52fedda3561f22ad43552cc906b53f8d4d0cbada
Nahedson <nahedson@gmail.com>
2017-07-24 11:49:38 -0300

    Modificado segundo o tutorial do angular. Não aparece nada. https://angular.io/tutorial/toh-pt5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,28 @@ import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 
 import { HeroDetailComponent } from './hero-detail.component';
-
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
 import { AppComponent } from './app.component';
+import { DashboardComponent }  from './dashboard.component';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    HeroService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
